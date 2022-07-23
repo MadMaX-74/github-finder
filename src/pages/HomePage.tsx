3f9ff77533197fd636585aff12b6278086131464
madmax-74 <maximus3506@mail.ru>
2022-07-24 00:39:38 +0300

    doing homePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {useSearchUSersQuery} from "./../store/github/github.api"
 
 export function HomePage(){
+    const [search, setSearch] = useState('')
     const {isLoading, isError, data} = useSearchUSersQuery('vladilen')
+
+    useEffect(() => {
+        console.log(search)
+    }, [search])
 
     
     return(
         <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
             {isError && <p className="text-center text-red-500">Somthing went wrong...</p>}
-            <div className='relative w-[560]'>
-                <input type="text" className='border py-2 px-2' />
+            
+            <div className='relative w-[560px]'>
+                <input type="text" className='border py-2 px-2 w-full h-[42px] mb-2' placeholder='Search for Github Username...' value={search} onChange={e => setSearch(e.target.value)}/>
+
+                <div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, commodi.</div>
             </div>
         </div>
     )
